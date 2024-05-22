0ade1523228b0e31d178e25c9d35cc91d32a8e81
S3annnyyy <seanyckang@gmail.com>
2024-05-22 15:32:53 +0800

    Initialize frontend react-vite-ts framework

---
+++ Frontend/src/utils/motion.ts
@@ -0,0 +1,68 @@
+export function slideInFromLeft(delay: number) {
+    return {
+      hidden: { x: -100, opacity: 0},
+      visible: {
+        x: 0,
+        opacity: 1,
+        top: 0,
+        transition: {
+          delay: delay,
+          duration: 0.5,
+        },
+      },
+      exit: {x: -100, opacity: 0, transition: { delay: 0.5, duration: 0.5 } }
+    };
+  }
+  
+  export function slideInFromRight(delay: number) {
+    return {
+      hidden: { x: 100, opacity: 0 },
+      visible: {
+        x: 0,
+        opacity: 1,
+        transition: {
+          delay: delay,
+          duration: 0.5,
+        },
+      },
+    };
+  }
+  
+  export const slideInFromTop = {
+    hidden: { y: -100, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        delay: 0.5,
+        duration: 0.5,
+      },
+    },
+  };
+
+  export const slideInFromBottom = {
+    hidden: { y: 100, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        delay: 0.5,
+        duration: 0.5,
+      },
+    },
+  };
+
+  export const dropInFromTop = {
+    hidden: {y: "-100vh"},
+    visible: {
+      y: "0",
+      opacity: 1,
+      transition: {
+        duration: 0.1,
+        type: "spring",
+        daming: 25,
+        stiffness: 500
+      }
+    },
+    exit: {y: "100vh"},
+  }
