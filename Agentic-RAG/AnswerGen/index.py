9ebc00f2a1f9ef357c5920bbf4e0232fc5e943f7
S3annnyyy <seanyckang@gmail.com>
2024-05-28 22:48:33 +0800

    feat: Added AnswerGeneration node component when document is valid

---
+++ Agentic-RAG/AnswerGen/index.py
@@ -0,0 +1,22 @@
+import langchain_community.chat_models.ollama as llmType
+import langchain_core.vectorstores as rtrType 
+from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+ANSGEN_PROMPT = PromptTemplate(
+    template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|> You are an assistant for question-answering tasks.
+    Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know.
+    Use three sentences maximum and keep the answer concise <|eot_id|><|start_header_id|>user<|end_header_id|>
+    Question: {question}
+    Context: {context}
+    Answer: <|eot_id|><|start_header_id|>assistant<|end_header_id|>""",
+    input_variables=["question", "document"],
+)
+
+def generateResponse(retriever: rtrType.VectorStoreRetriever, llm: llmType.ChatOllama, question: str):
+    """
+    """
+    rag_chain = ANSGEN_PROMPT | llm | StrOutputParser()
+    docs = retriever.invoke(question)
+    response = rag_chain.invoke({"context": docs, "question": question})
+    return response
--- Agentic-RAG/main.py
+++ Agentic-RAG/main.py
@@ -5,18 +5,23 @@ from langchain_community.vectorstores.utils import filter_complex_metadata
 from langchain_community.chat_models import ChatOllama
 from VectorDB import createVectorDB
 from RetrievalGrader import retrievalGrader
+from AnswerGen import generateResponse
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 URLS = ["https://towardsdatascience.com/text-classification-sentiment-analysis-on-r-sgexams-4ea341134fba"]
 FIRECRAWL_API_KEY = os.environ.get("FIRECRAWL_API_KEY")
 
-LLM = ChatOllama(model="llama3", format="json", temperature=0)
+GRADER_LLM = ChatOllama(model="llama3", format="json", temperature=0)
+GEN_LLM = ChatOllama(model="llama3", temperature=0)
 
 def main():
     retriever = createVectorDB(URLS, 250, 0, FIRECRAWL_API_KEY)  
-    output = retrievalGrader(retriever=retriever, llm=LLM, question="What is sentiment analysis of Secondary school?")
+    # output = retrievalGrader(retriever=retriever, llm=GRADER_LLM, question="What is sentiment analysis of Secondary school?")
+    # print(output)
+
+    output = generateResponse(retriever=retriever, llm=GEN_LLM, question="What is sentiment analysis of Secondary school?")
     print(output)
-    
+
 if __name__ == "__main__":
     main()
