4bf0dfd378f5ec5ff7856367fc181a23c7de57b5
S3annnyyy <seanyckang@gmail.com>
2024-06-02 17:33:13 +0800

    feat: Added langchain workflow

--- Agentic-RAG/main.py
+++ Agentic-RAG/main.py
@@ -1,17 +1,15 @@
 import os
 import logging
+from pprint import pprint
 from os.path import join, dirname
 from dotenv import load_dotenv
 from typing_extensions import TypedDict
 from typing import List
-from langchain.schema import Document
 from langgraph.graph import END, StateGraph
-from langchain_community.vectorstores.utils import filter_complex_metadata
-from langchain_community.chat_models import ChatOllama
 from VectorDB import createVectorDB
-from Grader import retrievalGrader, hallucinationGrader, answerGrader
-from AnswerGen import generateResponse
-from WebSearch import tavilySearchTool
+from Grader import retrievalGrader, hallucinationGrader
+from AnswerGen import generateResponse, decideToGenerate
+from WebSearch import tavilyWebSearchTool
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename='state.log', level=logging.INFO)    
@@ -20,21 +18,6 @@ load_dotenv(dotenv_path)
 URLS = ["https://towardsdatascience.com/text-classification-sentiment-analysis-on-r-sgexams-4ea341134fba"]
 FIRECRAWL_API_KEY = os.environ.get("FIRECRAWL_API_KEY")
 TAVILLY_API_KEY = os.environ.get("TAVILY_API_KEY")
-GEN_LLM = ChatOllama(model="llama3", temperature=0)
-
-# def main():
-#     retriever = createVectorDB(URLS, 250, 0, FIRECRAWL_API_KEY)  
-#     output = retrievalGrader(retriever=retriever, question="What is sentiment analysis of Secondary school?")
-#     print(output)
-
-#     output, docs = generateResponse(retriever=retriever, llm=GEN_LLM, question="How to buy iphone 5")
-#     print(output)
-
-#     output2 = hallucinationGrader(output, docs)
-#     print(output2)
-
-#     output3 = answerGrader(output, docs)
-#     print(output3)
 
 class LangGraphState(TypedDict):
     """
@@ -51,8 +34,63 @@ class LangGraphState(TypedDict):
     web_search: str
     documents: List[str]
 
+def retrieve(state: dict):
+    """
+    This function retrieve documents from vectorstore
+
+    Args:
+        state: The current graph state
+        retriever: retriever tool from vectorDB
+        
+    Returns:
+        state: New key added to state, documents that contains retrieved documents
+    """
+    retriever = createVectorDB(URLS, 250, 0, FIRECRAWL_API_KEY)
+    print("---RETRIEVE---")
+    question = state["question"]
+    documents = retriever.invoke(question)
+    return {"documents": documents, "question": question}
+
 def main():
-    raise NotImplementedError
+    workflow = StateGraph(LangGraphState)
+
+    # Define nodes
+    workflow.add_node("websearch", tavilyWebSearchTool)
+    workflow.add_node("retrieve", retrieve)  
+    workflow.add_node("grade_documents", retrievalGrader)
+    workflow.add_node("generate", generateResponse)
+
+    # Build graph
+    workflow.set_entry_point("retrieve")
+    workflow.add_edge("retrieve", "grade_documents")
+    workflow.add_conditional_edges(
+        "grade_documents",
+        decideToGenerate,
+        {
+            "websearch": "websearch",
+            "generate": "generate"
+        },
+    )
+    workflow.add_edge("websearch", "generate")
+    workflow.add_conditional_edges(
+        "generate",
+        hallucinationGrader,
+        {
+            "not_supported": "generate",
+            "useful": END,
+            "not useful": "websearch",
+        }
+    )
+
+    # COMPILE
+    app = workflow.compile()
+
+    # TESTING RAG
+    inputs = {"question": "How to save LLM cost"}
+    for output in app.stream(inputs):
+        for key, value in output.items():
+            pprint(f"Finished running: {key}")
+    pprint(value["generation"])
 
 if __name__ == "__main__":
     main()
